5e3332d99dcc54fefc3a8c56e95474393c13978c
Álvaro Villalba Navarro <alvaro.villalba@bsc.es>
2015-10-08 15:08:41 +0200

    create repository kafka

--- create_repository.sh
+++ create_repository.sh
@@ -77,4 +77,13 @@ else
 	echo Verified file: $ZK_FILE
 fi
 
+count=`md5sum -c $KAFKA_FILE_MD5 | grep -v OK | wc -l`
+if [ $count -gt 0 ]
+then
+ 	echo Corrupt or missing file found. Downloading $KAFKA_FILE_WGET/$KAFKA_FILE
+	wget -q $KAFKA_FILE_WGET/$KAFKA_FILE -O $FILE_REPOSITORY/$ZK_FILE
+else
+	echo Verified file: $KAFKA_FILE
+fi
+
 cd $ROOT
