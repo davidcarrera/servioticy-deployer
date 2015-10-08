d83631e654132a10be2bc6a45df161c92b8b023a
Álvaro Villalba Navarro <alvaro.villalba@bsc.es>
2015-10-08 09:18:02 +0200

    bugfix zk installation

--- scripts/install_zk.sh
+++ scripts/install_zk.sh
@@ -18,10 +18,10 @@ servercount=1
 while IFS='' read -r line || [[ -n "$line" ]]; do
     zkserversconf="$zkserversconf\nserver.$servercount=$line:2888:3888"
     servercount=servercount + 1
-done < "$MACHINE_FILE_ZK"
+done < "$MACHINE_FILES_FOLDER/$MACHINE_FILE_ZK"
 
 cat $ZK_CONF_TEMPLATE_FILE | \
 perl -pe "s/%PLACEHOLDER_ZK_SERVERS%/$zkserversconf/g" | \
-perl -pe "s/%PLACEHOLDER_ZK_DATA_DIR%/$ZK_DATA_DIR/g" > $ZK_INSTALL_DIR/conf/elasticsearch.yml
+perl -pe "s/%PLACEHOLDER_ZK_DATA_DIR%/$ZK_DATA_DIR/g" > $ZK_INSTALL_DIR/conf/zoo.cfg
 
 cd $ROOT
