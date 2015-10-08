e456b67ae8ac7e5f46a1311996be2ea64265a1de
Álvaro Villalba Navarro <alvaro.villalba@bsc.es>
2015-10-08 15:04:07 +0200

    stop kafka and zk

---
+++ scripts/stop_zk.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for pid in `ps -fA  | grep -e zk | grep -v grep |  tr -s " " | tr -d "\t" | perl -pe "s/^[ ]//" | cut -d " "  -f 2`
+do
+   kill -9 $pid &> /dev/null
+   wait $pid 2>/dev/null
+done
