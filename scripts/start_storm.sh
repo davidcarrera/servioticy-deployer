9899d93ad0f7574d0e68b89c73640ebaab9dfb19
Álvaro Villalba Navarro <alvaro.villalba@bsc.es>
2015-10-09 13:23:35 +0200

    run storm on background

--- scripts/start_storm.sh
+++ scripts/start_storm.sh
@@ -11,12 +11,12 @@ fi
 cd $STORM_INSTALL_DIR
 
 if [ -f $SERVIOTICY_INSTALL_DIR/storm_nimbus ]; then
-    nohup ./bin/storm nimbus
-    nohup ./bin/storm ui
+    nohup ./bin/storm nimbus &
+    nohup ./bin/storm ui &
 fi
 
 if [ -f $SERVIOTICY_INSTALL_DIR/storm_supervisor ]; then
-    nohup ./bin/storm supervisor
+    nohup ./bin/storm supervisor &
 fi
 
 cd $ROOT
