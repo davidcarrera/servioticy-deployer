c3c381afd395b759fceacf0dc68e9141d3feff43
Álvaro Villalba Navarro <alvaro.villalba@bsc.es>
2015-10-09 12:31:01 +0200

    yaml array indented

--- scripts/install_storm.sh
+++ scripts/install_storm.sh
@@ -18,7 +18,7 @@ ln -s $folder $STORM_INSTALL_DIR
 
 zkservers=""
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    zkservers="$zkservers- $line\n"
+    zkservers="$zkservers  - $line\n"
 done < "$MACHINE_FILES_FOLDER/$MACHINE_FILE_ZK"
 
 nimbushost=$(head -n 1 $MACHINE_FILES_FOLDER/$MACHINE_FILE_STORM)
