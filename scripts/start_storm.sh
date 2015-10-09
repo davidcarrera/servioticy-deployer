10aedd74129106d3b19b86044686b09d1daba393
Álvaro Villalba Navarro <alvaro.villalba@bsc.es>
2015-10-09 15:33:35 +0200

    storm start now working

--- scripts/start_storm.sh
+++ scripts/start_storm.sh
@@ -11,13 +11,13 @@ fi
 cd $STORM_INSTALL_DIR
 
 if [ -f $SERVIOTICY_INSTALL_DIR/storm_nimbus ]; then
-    nohup ./bin/storm nimbus &
-    nohup ./bin/storm ui &
+    nohup ./bin/storm nimbus `</dev/null` > $SERVIOTICY_INSTALL_DIR/stormnimbus.out 2>&1 &
+    nohup ./bin/storm ui `</dev/null` > $SERVIOTICY_INSTALL_DIR/stormgui.out 2>&1 &
     exit
 fi
 
 if [ -f $SERVIOTICY_INSTALL_DIR/storm_supervisor ]; then
-    nohup ./bin/storm supervisor &
+    nohup ./bin/storm supervisor `</dev/null` > $SERVIOTICY_INSTALL_DIR/stormsupervisor.out 2>&1 &
     exit
 fi
 
