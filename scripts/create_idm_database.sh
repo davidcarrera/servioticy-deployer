bd71b90bbd4c6d00331ee3e0e6b0d767bf8fcf47
Álvaro Villalba Navarro <alvaro.villalba@bsc.es>
2015-10-13 18:39:09 +0200

    minor bugfix

--- scripts/create_idm_database.sh
+++ scripts/create_idm_database.sh
@@ -10,4 +10,4 @@ fi
 
 cd $MYSQL_INSTALL_DIR
 
-./bin/mysql --socket=$MYSQL_INSTALL_DIR/mysql.sock -uroot -p -e "CREATE DATABASE IF NOT EXISTS composeidentity2;"
+./bin/mysql --socket=$SERVIOTICY_INSTALL_DIR/mysql.sock -uroot -p -e "CREATE DATABASE IF NOT EXISTS composeidentity2;"
