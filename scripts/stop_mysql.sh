39113c236ed1ef52864c4bab96f0cedbb2ed04c6
Álvaro Villalba Navarro <alvaro.villalba@bsc.es>
2015-10-13 19:11:29 +0200

    minor fix

--- scripts/stop_mysql.sh
+++ scripts/stop_mysql.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+if [ -z "$ROOT" ]
+then
+    ROOT=$(while ! test -e env.sh.sample; do cd ..; done; pwd)
+    export ROOT
+fi
+
+. $ROOT/env.sh
+
 cd $MYSQL_INSTALL_DIR
 ./bin/mysqladmin -h 127.0.0.1 -P 3307 -u root shutdown
+cd $ROOT
