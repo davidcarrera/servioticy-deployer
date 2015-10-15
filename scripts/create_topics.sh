434f37d4704cfcd7c9124aeb9928aba5c2135c44
Álvaro Villalba Navarro <alvaro.villalba@bsc.es>
2015-10-15 20:20:31 +0200

    commented unnecessary topics

--- scripts/create_topics.sh
+++ scripts/create_topics.sh
@@ -11,8 +11,8 @@ fi
 cd $KAFKA_INSTALL_DIR
 zk_machine=$(head -n 1 $MACHINE_FILES_FOLDER/$MACHINE_FILE_ZK)
 
-./bin/kafka-topics.sh --zookeeper $zk_machine:2181 --create --topic reputation --partitions 8 --replication-factor 1 --config retention.ms=120000
+#./bin/kafka-topics.sh --zookeeper $zk_machine:2181 --create --topic reputation --partitions 8 --replication-factor 1 --config retention.ms=120000
 ./bin/kafka-topics.sh --zookeeper $zk_machine:2181 --create --topic updates --partitions 8 --replication-factor 1 --config retention.ms=120000
-./bin/kafka-topics.sh --zookeeper $zk_machine:2181 --create --topic actions --partitions 8 --replication-factor 1 --config retention.ms=120000
+#./bin/kafka-topics.sh --zookeeper $zk_machine:2181 --create --topic actions --partitions 8 --replication-factor 1 --config retention.ms=120000
 
 cd $ROOT
