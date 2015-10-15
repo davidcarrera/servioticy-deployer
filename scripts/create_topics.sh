330783e3d29fe7edb7d586eb90ed5f74d9d87464
Álvaro Villalba Navarro <alvaro.villalba@bsc.es>
2015-10-15 20:41:33 +0200

    minor fix

--- scripts/create_topics.sh
+++ scripts/create_topics.sh
@@ -11,6 +11,8 @@ fi
 cd $KAFKA_INSTALL_DIR
 zk_machine=$(head -n 1 $MACHINE_FILES_FOLDER/$MACHINE_FILE_ZK)
 
+echo "Creating kafka topics"
+
 #./bin/kafka-topics.sh --zookeeper $zk_machine:2181 --create --topic reputation --partitions 8 --replication-factor 1 --config retention.ms=120000
 ./bin/kafka-topics.sh --zookeeper $zk_machine:2181 --create --topic updates --partitions 8 --replication-factor 1 --config retention.ms=120000
 #./bin/kafka-topics.sh --zookeeper $zk_machine:2181 --create --topic actions --partitions 8 --replication-factor 1 --config retention.ms=120000
