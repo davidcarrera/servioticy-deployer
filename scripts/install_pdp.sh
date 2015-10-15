c4ec9d6fbe36dc165994d1c7941b9ad9cde15e87
Álvaro Villalba Navarro <alvaro.villalba@bsc.es>
2015-10-15 12:53:19 +0200

    minor fix

--- scripts/install_pdp.sh
+++ scripts/install_pdp.sh
@@ -11,11 +11,10 @@ fi
 echo "Installing PDP"
 mkdir -p $SERVIOTICY_INSTALL_DIR
 
-cp -R $FILE_REPOSITORY/pdp $PDP_INSTALL_DIR/
-cd $PDP_INSTALL_DIR/resources/main/lib/
+cd $FILE_REPOSITORY/pdp/resources/main/lib/
 mvn install:install-file -Dfile=IoTP-0.1.0.jar -DpomFile=pom.xml
 
-cd $PDP_INSTALL_DIR/
+cd $FILE_REPOSITORY/pdp/
 gradle install
 
 cd $ROOT
