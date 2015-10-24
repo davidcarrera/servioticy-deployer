c9bfc7df4c0ea463df5340a13d55007f779f110c
davidcarrera <dcarrera@ac.upc.edu>
2015-10-25 00:59:52 +0200

    JDK detection not exclusive for 1.8

--- scripts/install_jdk.sh
+++ scripts/install_jdk.sh
@@ -12,6 +12,6 @@ echo "Installing JDK"
 mkdir -p $SERVIOTICY_INSTALL_DIR
 cd $SERVIOTICY_INSTALL_DIR
 tar xzf $FILE_REPOSITORY/$JDK8_FILE
-folder=`ls -1 | grep jdk1.8 | head -1`
+folder=`ls -1 | grep jdk | head -1`
 ln -s $folder $JDK8_INSTALL_DIR
 cd $ROOT
