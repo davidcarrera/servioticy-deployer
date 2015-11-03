75c00c2f7610254c00e08256ad6ef40b14ab7a31
Álvaro Villalba Navarro <alvaro.villalba@bsc.es>
2015-11-03 13:12:11 +0100

    stopping the dispatcher and readding datasource.properties

--- scripts/install_idm.sh
+++ scripts/install_idm.sh
@@ -25,7 +25,7 @@ perl -pe "s|%PLACEHOLDER_IDM_PIP_URL%|http://$pipurl|g" > ./src/main/resources/p
 cat $IDM_SERVIOTICY_TEMPLATE_FILE | \
 perl -pe "s|%PLACEHOLDER_IDM_SERVIOTICY_PRIVATE_URL%|http://$jettyurl:8080/private/security/|g" > ./src/main/resources/servioticy.properties
 
-#cp $IDM_DATASOURCE_TEMPLATE_FILE ./src/main/resources/datasource.properties
+cp $IDM_DATASOURCE_TEMPLATE_FILE ./src/main/resources/datasource.properties
 #cp $IDM_UAA_TEMPLATE_FILE ./src/main/resources/uaa.properties
 
 ./compile_jar.sh
